b0c971f01c4cd7f6d3dfa1d7190df92c2aa1ba66
Insafprog <insaf@gmx.ru>
2022-05-07 21:07:26 +0300

    feat: add event listener keyboard

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let keys = []
 
-keys = ["ё", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "Backspace", "Tab", "Й", "Ц", "У", "К", "Е", "Н", "Г", "Ш", "Щ", "З", "Х", "Ъ", "\\", "Del", "Caps Lock", "Ф", "Ы", "В", "А", "П", "Р", "О", "Л", "Д", "Ж", "Э", "Enter", "Shift", "Я", "Ч", "С", "М", "И", "Т", "Ь", "Б", "Ю", ".", "a_top", "Shift", "Ctrl", "Win", "Alt", "Space", "Alt", "Win", "a_left", "a_bottom", "a_right", "Ctrl"]
+keys = ["Ё", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "Backspace", "Tab", "Й", "Ц", "У", "К", "Е", "Н", "Г", "Ш", "Щ", "З", "Х", "Ъ", "\\", "Del", "CapsLock", "Ф", "Ы", "В", "А", "П", "Р", "О", "Л", "Д", "Ж", "Э", "Enter", "Shift", "Я", "Ч", "С", "М", "И", "Т", "Ь", "Б", "Ю", ".", "ArrowUp", "Shift", "Ctrl", "Win", "Alt", "Space", "Alt", "Win", "ArrowLeft", "ArrowDown", "ArrowRight", "Ctrl"]
 
 const keyboard = document.createElement('div')
 keyboard.id = "keyboard"
@@ -14,3 +14,20 @@ keys.forEach(k => {
 })
 
 document.body.appendChild(keyboard)
+
+keyboard_ul.querySelectorAll(".key_win").forEach(k => k.innerHTML = '<i class="fa fa-windows" aria-hidden="true"></i>')
+keyboard_ul.querySelector(".key_arrowup").innerHTML = '<i class="fa fa-caret-up" aria-hidden="true"></i>'
+keyboard_ul.querySelector(".key_arrowdown").innerHTML = '<i class="fa fa-caret-down" aria-hidden="true"></i>'
+keyboard_ul.querySelector(".key_arrowleft").innerHTML = '<i class="fa fa-caret-left" aria-hidden="true"></i>'
+keyboard_ul.querySelector(".key_arrowright").innerHTML = '<i class="fa fa-caret-right" aria-hidden="true"></i>'
+keyboard_ul.querySelector(".key_capslock").innerHTML = "Caps Lock"
+
+keyboard_ul.querySelectorAll("li").forEach(li => {
+    li.addEventListener('mousedown', function(event){
+        li.classList.add("active")
+    })
+    li.addEventListener('mouseup', function(event){
+        li.classList.remove("active")
+        document.querySelector("textarea").value += li.innerHTML
+    })
+})
